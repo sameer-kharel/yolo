469d058e8ee9a77d15ccb238da3b9ecbf660424f
shameer kharel <126055775+sameer-kharel@users.noreply.github.com>
2023-10-29 12:44:09 +0545

    Update objects.py

--- objects.py
+++ objects.py
@@ -6,14 +6,7 @@ from ultralytics import YOLO
 
 # Load a pretrainied yolov8n model
 model = YOLO("yolov8m.pt")
-# path = " ya chai path hala image ya video ko"
 
-
-
-# yo chai camera ko lagi ho camera kholinxa
+# to open tha camera
 results = model.track(source=0, show=True, tracker="bytetrack.yaml")
 
-
-
-# yo chai path hale si chalau mathi ko ma comment banara 
-# results = model.track(source=path, show=True, tracker="bytetrack.yaml")
